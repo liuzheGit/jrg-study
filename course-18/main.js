6983f1fd82cf3821672515ff61372bd921f21ce1
liuzhe <zhethink@gmail.com>
2019-06-10 18:15:37 +0800

    测试改变画笔的宽度

--- course-18/main.js
+++ course-18/main.js
@@ -130,7 +130,7 @@ function drawCircle(x, y) {
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
   context.moveTo(x1, y1);
-  context.lineWidth = 5;
+  context.lineWidth = 20;
   context.lineTo(x2, y2);
   context.stroke();
   context.closePath();
