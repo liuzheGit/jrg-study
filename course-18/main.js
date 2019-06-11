997a7af748816d72ebeea548ec88d1a506e780b2
liuzhe <zhethink@gmail.com>
2019-06-11 09:50:59 +0800

    添加改变画笔粗细, 清除画布, 保存为图片功能.

--- course-18/main.js
+++ course-18/main.js
@@ -1,13 +1,32 @@
 let canvas = document.getElementById('canvas');
+let globalWidth = 3;
 
 autoSetCanvasSize(canvas);
-let buttons = document.getElementById('buttons');
 let eraser = document.getElementById('eraser');
 let brush = document.getElementById('brush');
-
-listenUser(canvas);
+let clear = document.getElementById('clear');
+let save = document.getElementById('save');
 let context = canvas.getContext('2d');
 let isCleaning = false;
+
+// 清除画布
+clear.onclick = function(){
+  context.clearRect(0,0,canvas.width, canvas.height);
+};
+
+// 保存为png
+save.onclick = function(){
+  let strDataURI = canvas.toDataURL("image/png");
+  // 创建a链接用来下载图片资源
+  let a = document.createElement('a');
+  a.href = strDataURI;
+  a.download = 'myCanvas';
+  a.target = '_blank';
+  a.click();
+};
+
+// 监听用户点画画
+listenUser(canvas);
 
 // 画笔和橡皮擦切换
 brush.onclick = function () {
@@ -20,17 +39,35 @@ eraser.onclick = function () {
   eraser.classList.add('active');
   brush.classList.remove('active');
 };
+
+
+// 切换画笔粗细
+let thickness = document.querySelector('.thickness');
+thickness.addEventListener('click', function(e){
+  let nessSize = e.target.getAttribute('ness');
+  if(nessSize){
+    globalWidth = nessSize;
+    let nessLists = [].slice.call(this.children);
+    nessLists.forEach(function(item, index, arr){
+      item.classList.remove('active');
+    });
+    e.target.classList.add('active')
+  }
+});
+
 // 切换颜色
 let colors = document.querySelector('.colors');
 colors.addEventListener('click',function(e){
   let colorName = e.target.getAttribute('color');
-  context.fillStyle = colorName;
-  context.strokeStyle = colorName;
-  let colorLists = [].slice.call(this.children);
-  colorLists.forEach(function(item, index, arr){
-    item.classList.remove('active')
-  });
-  e.target.classList.add('active');
+  if(colorName){
+    context.fillStyle = colorName;
+    context.strokeStyle = colorName;
+    let colorLists = [].slice.call(this.children);
+    colorLists.forEach(function(item, index, arr){
+      item.classList.remove('active')
+    });
+    e.target.classList.add('active');
+  }
 });
 
 function listenUser(canvas) {
@@ -46,7 +83,7 @@ function listenUser(canvas) {
       if (isCleaning) {
         context.clearRect(x - 5, y - 5, 10, 10);
       } else {
-        drawCircle(x, y);
+        // drawCircle(x, y);
       }
       lastPoint = {
         'x': x,
@@ -66,7 +103,7 @@ function listenUser(canvas) {
       if (isCleaning) {
         context.clearRect(x, y, 10, 10);
       } else {
-        drawCircle(x, y);
+        // drawCircle(x, y);
         drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
       }
       lastPoint = newPoint;
@@ -84,7 +121,7 @@ function listenUser(canvas) {
       if (isCleaning) {
         context.clearRect(x - 5, y - 5, 10, 10);
       } else {
-        drawCircle(x, y);
+        // drawCircle(x, y);
       }
       lastPoint = {
         'x': x,
@@ -106,7 +143,7 @@ function listenUser(canvas) {
       if (isCleaning) {
         context.clearRect(x, y, 10, 10);
       } else {
-        drawCircle(x, y);
+        // drawCircle(x, y);
         drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
       }
       lastPoint = newPoint;
@@ -130,7 +167,7 @@ function drawCircle(x, y) {
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
   context.moveTo(x1, y1);
-  context.lineWidth = 20;
+  context.lineWidth = globalWidth;
   context.lineTo(x2, y2);
   context.stroke();
   context.closePath();
