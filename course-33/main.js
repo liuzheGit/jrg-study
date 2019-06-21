63e1b1c92589e8da23c75e23a1fe275c00f856e9
liuzhe <zhethink@gmail.com>
2019-06-21 15:23:39 +0800

    用函数遍历点击事件, 添加多少个都可以做到

--- course-33/main.js
+++ course-33/main.js
@@ -8,42 +8,25 @@ let $imgLast = $allImgs.eq($allImgs.length - 1).clone(true);
 $slideWrap.append($imgFirst).prepend($imgLast);
 $slideWrap.css('transform', `translateX(-${WIDTH}px)`);
 
-$('#btn1').on('click', function(e){
-  let index = $(e.currentTarget).index() + 1;
-  let lastIndex = $allImgs.length;
-  if(n === lastIndex){
-    $slideWrap.css({
-      'transform': `translateX(-${(lastIndex + 1) * WIDTH}px)`
-    }).one('transitionend',function(){
-      console.log('动画结束');
-      $slideWrap.hide().offset();
-      $slideWrap.css('transform', `translateX(-${WIDTH}px)`).show()
-    });
-  }else{
-    $slideWrap.css({
-      'transform': `translateX(-${WIDTH}px)`
-    });
-  }
-  n = index;
+$('.buttons').on('click', 'button',function(e){
+    let index = $(e.currentTarget).index();
+    if(n === $allImgs.length && index === 0){
+      $slideWrap.css({
+        'transform': `translateX(-${($allImgs.length + 1) * WIDTH}px)`
+      }).one('transitionend', function(){
+        $slideWrap.hide().offset();
+        $slideWrap.css('transform', `translateX(-${WIDTH}px)`).show()
+      })
+    }else if(n === 1 && index + 1 === $allImgs.length){
+      $slideWrap.css({
+        'transform': `translateX(0)`
+      }).one('transitionend',function(){
+        $slideWrap.hide().offset();
+        $slideWrap.css('transform', `translateX(-${$allImgs.length * WIDTH}px)`).show()
+      });
+    }else {
+      $slideWrap.css( 'transform', `translateX(-${WIDTH * (index + 1)}px)` );
+    }
+    n = index + 1;
 });
-$('#btn2').on('click', function(e){
-  let index = $(e.currentTarget).index() + 1;
-  $slideWrap.css( 'transform', 'translateX(-800px)' );
-  n = index;
-});
-$('#btn3').on('click', function(e){
-  let index = $(e.currentTarget).index() + 1;
-  if(n === 1){
-    $slideWrap.css({
-      'transform': 'translateX(0)'
-    }).one('transitionend',function(){
-      $slideWrap.hide().offset();
-      $slideWrap.css('transform', 'translateX(-1200px)').show()
-    });
-  }else{
-    $slideWrap.css({
-      'transform': 'translateX(-1200px)'
-    });
-    n = index;
-  }
-});
+
