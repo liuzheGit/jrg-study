3a50c6bd3a4b7377ab2babbb0a01c73b5afa57e1
liuzhe <zhethink@gmail.com>
2019-06-21 15:53:17 +0800

    分离出来gotoSlide, 这样可以很重要

--- course-33/main.js
+++ course-33/main.js
@@ -1,7 +1,9 @@
-let n = 1;
+let current = 0;
+let temer;
 let $allImgs = $('.slide-wrap > img');
 let $slideWrap = $('.slide-wrap');
 let WIDTH = $allImgs.width();
+let m = 1;
 
 let $imgFirst = $allImgs.eq(0).clone(true);
 let $imgLast = $allImgs.eq($allImgs.length - 1).clone(true);
@@ -10,23 +12,39 @@ $slideWrap.css('transform', `translateX(-${WIDTH}px)`);
 
 $('.buttons').on('click', 'button',function(e){
     let index = $(e.currentTarget).index();
-    if(n === $allImgs.length && index === 0){
-      $slideWrap.css({
-        'transform': `translateX(-${($allImgs.length + 1) * WIDTH}px)`
-      }).one('transitionend', function(){
-        $slideWrap.hide().offset();
-        $slideWrap.css('transform', `translateX(-${WIDTH}px)`).show()
-      })
-    }else if(n === 1 && index + 1 === $allImgs.length){
-      $slideWrap.css({
-        'transform': `translateX(0)`
-      }).one('transitionend',function(){
-        $slideWrap.hide().offset();
-        $slideWrap.css('transform', `translateX(-${$allImgs.length * WIDTH}px)`).show()
-      });
-    }else {
-      $slideWrap.css( 'transform', `translateX(-${WIDTH * (index + 1)}px)` );
-    }
-    n = index + 1;
+    goToSlide(index)
+});
+$('#next').on('click',function(){
+    goToSlide(current + 1)
+});
+$('#previous').on('click',function(){
+    goToSlide(current - 1)
 });
 
+function goToSlide(index){
+  if(index > $allImgs.length - 1){
+    index = 0
+  }else if(index < 0){
+    index = $allImgs.length - 1
+  }
+    if(current === $allImgs.length - 1 && index === 0){
+        // 从最后一张到第一张
+        $slideWrap.css({
+            'transform': `translateX(-${($allImgs.length + 1) * WIDTH}px)`
+        }).one('transitionend', function(){
+            $slideWrap.hide().offset();
+            $slideWrap.css('transform', `translateX(-${WIDTH}px)`).show()
+        })
+    }else if(current === 0 && index + 1 === $allImgs.length){
+      // 从第一张到最后一张
+        $slideWrap.css({
+            'transform': `translateX(0)`
+        }).one('transitionend',function(){
+            $slideWrap.hide().offset();
+            $slideWrap.css('transform', `translateX(-${$allImgs.length * WIDTH}px)`).show()
+        });
+    }else {
+        $slideWrap.css( 'transform', `translateX(-${WIDTH * (index + 1)}px)` );
+    }
+    current = index;
+}
