66eb577adcd59554b21872a53141a0b02bc7e004
liuzhe <zhethink@gmail.com>
2019-06-25 10:46:19 +0800

    文件都放到src里归类, 把cdn转为本地的js, 加入swiper

--- course-7/src/js/main.js
+++ course-7/src/js/main.js
@@ -1,28 +1,8 @@
-// 展示产品
-let stateDiv = document.getElementById('nav-progress-bar');
-let show = document.getElementById('show-works');
-let btn1 = document.getElementById('btn1');
-let btn2 = document.getElementById('btn2');
-let btn3 = document.getElementById('btn3');
-btn1.onclick = function(){
-  stateDiv.className = 'state1';
-  show.className = 'works state1';
-};
-btn2.onclick = function(){
-  stateDiv.className = 'state2';
-  show.className = 'works state2';
-};
-btn3.onclick = function(){
-  stateDiv.className = 'state3';
-  show.className = 'works state3';
-};
-
 // loading
 let loadingWrap = document.getElementById('loadingWrap');
 setTimeout(function () {
   loadingWrap.classList.remove('active');
-},500);
-
+},500)
 // 会变的头部
 let siteHead = document.getElementById('siteHead');
 // 标记的有特殊属性的标签
